c5678caa55d5cafe0e2bcab0c9094bf997ffb2da
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-09-16 10:58:22 +0100

    creation de classe Operation

--- src/entities/Operation.java
+++ src/entities/Operation.java
@@ -10,7 +10,36 @@ public abstract class Operation {
 	private LocalDateTime date ; 
 	private double montant ;
 	
+	//constructor
+	public Operation(UUID UUID , LocalDateTime date , double montant) {
+		this.UUID = UUID ;
+		this.date = date ;
+		this.montant = montant ;
+	}
 	
+	//getters
+	public UUID getUUID() {
+		return this.UUID;
+	}
 	
+	public LocalDateTime getDate() {
+		return this.date;
+	}
+	
+	public double getMontant() {
+		return this.montant;
+	}
+	
+	//setters
+	public void setUUID(UUID UUID) {
+		this.UUID = UUID ;
+	}
 
+	public void setMontant(double montant) {
+		this.montant = montant ;
+	}
+	
+	public void setDate(LocalDateTime date) {
+		this.date = date ;
+	}
 }
