702a1687e0cd96e515cc5c80aca123479647c12a
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-09-16 11:56:32 +0100

    ajouter getters et setters dans la classe Versement

--- src/entities/Versement.java
+++ src/entities/Versement.java
@@ -1,5 +1,26 @@
 package entities;
 
-public class Versement {
+import java.util.UUID ; 
 
+public class Versement extends Operation {
+	
+	//attribute 
+	String source ; 
+	
+	//constructor
+	public Versement(UUID UUID , double montant , LocalDateTime date , String source) {
+		super(UUID , montant , date);
+		this.source = source ;
+		
+	}
+	
+	//getters 
+	
+	public String getSource() {
+		return this.source ; 
+	}
+    
+	//setters
+	public void setSource(String source)
+	this.source = source ;
 }
