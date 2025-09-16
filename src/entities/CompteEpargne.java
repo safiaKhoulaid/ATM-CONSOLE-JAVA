2ba877c32d6dd1c458fdc711d0c9c11ec193747a
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-09-16 11:19:06 +0100

    creation de classe Compte Courant herite de classe parent Compte

--- src/entities/CompteEpargne.java
+++ src/entities/CompteEpargne.java
@@ -1,5 +1,15 @@
 package entities;
 
-public class CompteEpargne {
+public class CompteEpargne extends Compte{
+	
+	//attributes
+	
+	private int tauxInteret ; 
+	
+	//constructor 
+	public CompteEpargne(String code , double solde , int tauxInteret ) {
+		super(code , solde);
+		this.tauxInteret = tauxInteret ;
+	}
 
 }
